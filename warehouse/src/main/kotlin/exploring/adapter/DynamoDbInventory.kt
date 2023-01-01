26ac8c476d348570f81640d98e2cfa10ff8d80a1
David Denton <denton.david@gmail.com>
2023-01-01 21:33:09 +0000

    simplify usage of dynamo table mapper

--- warehouse/src/main/kotlin/exploring/adapter/DynamoDbInventory.kt
+++ warehouse/src/main/kotlin/exploring/adapter/DynamoDbInventory.kt
@@ -9,7 +9,6 @@ import exploring.port.Inventory
 import org.http4k.cloudnative.env.Environment
 import org.http4k.connect.amazon.dynamodb.DynamoDb
 import org.http4k.connect.amazon.dynamodb.Http
-import org.http4k.connect.amazon.dynamodb.mapper.DynamoDbTableMapperSchema.Primary
 import org.http4k.connect.amazon.dynamodb.mapper.tableMapper
 import org.http4k.connect.amazon.dynamodb.model.Attribute
 import org.http4k.connect.amazon.dynamodb.model.value
@@ -17,11 +16,10 @@ import org.http4k.core.HttpHandler
 
 fun Inventory.Companion.DynamoDb(env: Environment, http: HttpHandler) =
     object : Inventory {
-        private val index = Primary(Attribute.value(ItemId).required("id"), Attribute.string().required("name"))
-        private val table =
-            DynamoDb.Http(env, http).tableMapper<InventoryItem, ItemId, String>(INVENTORY_DB_TABLE(env), index)
+        private val table = DynamoDb.Http(env, http)
+            .tableMapper<InventoryItem, ItemId, Any>(INVENTORY_DB_TABLE(env), Attribute.value(ItemId).required("id"))
 
-        override fun items() = resultFrom { table.index(index).query() }.map { it.toList() }
+        override fun items() = resultFrom { table.primaryIndex().query() }.map { it.toList() }
 
         override fun store(item: InventoryItem) = resultFrom { table += item }
 
