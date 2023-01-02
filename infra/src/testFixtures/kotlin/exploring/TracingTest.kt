0d0fb434a2bb2ea162745136209b25622471ccae
David Denton <denton.david@gmail.com>
2023-01-02 23:42:02 +0000

    added more diagram generation

--- infra/src/testFixtures/kotlin/exploring/TracingTest.kt
+++ infra/src/testFixtures/kotlin/exploring/TracingTest.kt
@@ -3,10 +3,15 @@ package exploring
 import org.http4k.events.MetadataEvent
 import org.http4k.tracing.Actor
 import org.http4k.tracing.ActorResolver
-import org.http4k.tracing.ActorType
+import org.http4k.tracing.ActorType.System
 import org.http4k.tracing.TraceRenderPersistence
 import org.http4k.tracing.junit.TracerBulletEvents
 import org.http4k.tracing.persistence.FileSystem
+import org.http4k.tracing.renderer.MarkdownDocument
+import org.http4k.tracing.renderer.MarkdownTraceDepthTable
+import org.http4k.tracing.renderer.MarkdownTraceStepCountsTable
+import org.http4k.tracing.renderer.MermaidInteractionDiagram
+import org.http4k.tracing.renderer.MermaidSequenceDiagram
 import org.http4k.tracing.renderer.PumlInteractionDiagram
 import org.http4k.tracing.renderer.PumlSequenceDiagram
 import org.http4k.tracing.tracer.HttpTracer
@@ -17,11 +22,22 @@ abstract class TracingTest {
     @RegisterExtension
     val events = TracerBulletEvents(
         listOf(::HttpTracer, ::DbTracer).map { it((ActorByService)) },
-        listOf(PumlSequenceDiagram, PumlInteractionDiagram),
+        listOf(
+            PumlSequenceDiagram,
+            PumlInteractionDiagram,
+            MarkdownDocument(
+                MermaidSequenceDiagram,
+                MermaidInteractionDiagram,
+                MarkdownTraceDepthTable,
+                MarkdownTraceStepCountsTable
+            )
+        ),
         TraceRenderPersistence.FileSystem(File(".generated"))
     )
 }
 
 object ActorByService : ActorResolver {
-    override fun invoke(it: MetadataEvent) = Actor(it.metadata["service"]!!.toString(), ActorType.System)
+    override fun invoke(it: MetadataEvent) = Actor(
+        it.metadata["service"]!!.toString(), System
+    )
 }
