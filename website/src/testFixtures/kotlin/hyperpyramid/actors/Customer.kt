8933e4ae2423f0f23391bcde7dd8db0f66154877
David Denton <denton.david@gmail.com>
2023-01-05 21:24:50 +0000

    fix customer

--- website/src/testFixtures/kotlin/hyperpyramid/actors/Customer.kt
+++ website/src/testFixtures/kotlin/hyperpyramid/actors/Customer.kt
@@ -15,17 +15,17 @@ import org.http4k.webdriver.Http4kWebDriver
 import org.openqa.selenium.By
 
 class Customer(
-    http: HttpHandler,
+    rawHttp: HttpHandler,
     private val baseUri: Uri,
     private val emailInbox: Emails,
     events: Events = {}
-) : Actor("Website User", http, events) {
+) : Actor("Website User", rawHttp, events) {
 
     private val email = Email.of("joe@http4k.org")
 
-    private fun browser() = Http4kWebDriver(http)
+    private val browser = Http4kWebDriver(http)
 
-    fun loginAndListItems() = with(browser()) {
+    fun loginAndListItems() = with(browser) {
         navigate().to(baseUri)
         findElement(By.id("email"))!!.sendKeys(email.value)
         findElement(By.tagName("form"))!!.submit()
@@ -35,7 +35,7 @@ class Customer(
 
     fun canSeeImage(id: ItemId) = http(Request(GET, baseUri.extend(Uri.of("/img/$id")))).status == OK
 
-    fun order(id: ItemId): OrderId = with(browser()) {
+    fun order(id: ItemId): OrderId = with(browser) {
         findElement(By.id("ITEM$id"))?.submit()
         return OrderId.of(findElement(By.id("orderId"))!!.text.toInt())
     }
