0dd5a16e33554894812d597a7c6bb4ef6a9aa6d7
David <denton.david@gmail.com>
2023-02-22 22:58:44 +0000

    remove debug

--- website/src/test/kotlin/hyperpyramid/env/InMemoryWebsiteTests.kt
+++ website/src/test/kotlin/hyperpyramid/env/InMemoryWebsiteTests.kt
@@ -5,11 +5,10 @@ import hyperpyramid.TracingTest
 import hyperpyramid.Website
 import hyperpyramid.WebsiteTestEnv
 import hyperpyramid.scenario.BrowsingContract
-import org.http4k.filter.debug
 import org.http4k.testing.ApprovalTest
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(ApprovalTest::class)
 class InMemoryWebsiteTests : TracingTest(), BrowsingContract {
-    override val http = Website(WebsiteTestEnv, events, http = FakeWarehouse()).debug()
+    override val http = Website(WebsiteTestEnv, events, http = FakeWarehouse())
 }
