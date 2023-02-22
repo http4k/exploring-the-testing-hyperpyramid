4b4b626867e3c57cd7f7748c808dc8c502f403d4
David <denton.david@gmail.com>
2023-02-22 23:08:49 +0000

    tweak testcase

--- website/src/test/kotlin/hyperpyramid/env/InMemoryWebsiteTests.kt
+++ website/src/test/kotlin/hyperpyramid/env/InMemoryWebsiteTests.kt
@@ -5,12 +5,11 @@ import hyperpyramid.TracingTest
 import hyperpyramid.Website
 import hyperpyramid.WebsiteTestEnv
 import hyperpyramid.scenario.BrowsingContract
-import org.http4k.events.then
 import org.http4k.testing.ApprovalTest
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(ApprovalTest::class)
 class InMemoryWebsiteTests : TracingTest(), BrowsingContract {
 
-    override val http = Website(WebsiteTestEnv, events.then(::println), http = FakeWarehouse())
+    override val http = Website(WebsiteTestEnv, events, http = FakeWarehouse())
 }
