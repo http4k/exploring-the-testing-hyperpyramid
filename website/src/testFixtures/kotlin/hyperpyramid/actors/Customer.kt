a2ca1414b32b91ec34a59a834dfa2bd651d21024
David <denton.david@gmail.com>
2023-02-22 23:09:42 +0000

    tweak testcase

--- website/src/testFixtures/kotlin/hyperpyramid/actors/Customer.kt
+++ website/src/testFixtures/kotlin/hyperpyramid/actors/Customer.kt
@@ -14,27 +14,21 @@ import org.http4k.events.Events
 import org.http4k.webdriver.Http4kWebDriver
 import org.openqa.selenium.By
 
-class Customer(
-    rawHttp: HttpHandler,
-    private val baseUri: Uri,
-    private val emailInbox: Emails,
-    events: Events = {}
-) : Actor("Website User", rawHttp, events) {
-
-    private val email = Email.of("joe@http4k.org")
+class Customer(rawHttp: HttpHandler, private val baseUri: Uri, private val emailInbox: Emails, events: Events = {}) :
+    Actor("Website User", rawHttp, events) {
 
     private val browser = Http4kWebDriver(http)
-
-    fun login() = with(browser) {
-        navigate().to(baseUri)
-        findElement(By.id("email"))!!.sendKeys(email.value)
-        findElement(By.tagName("form"))!!.submit()
-    }
 
     fun listItems() = with(browser) {
         navigate().to(baseUri)
         (findElements(By.tagName("form")) ?: emptyList())
             .map { ItemId.of(it.getAttribute("action").substringAfterLast('/')) }
+    }
+
+    fun login() = with(browser) {
+        navigate().to(baseUri)
+        findElement(By.id("email"))!!.sendKeys(email.value)
+        findElement(By.tagName("form"))!!.submit()
     }
 
     fun canSeeImage(id: ItemId) = http(Request(GET, baseUri.extend(Uri.of("/img/$id")))).status == OK
@@ -47,4 +41,6 @@ class Customer(
 
     fun hasEmailFor(orderId: OrderId) = emailInbox(email)
         .any { it.second == "Please collect your order using the code: $orderId" }
+
+    private val email = Email.of("joe@http4k.org")
 }
