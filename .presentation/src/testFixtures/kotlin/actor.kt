90915a93b70fd2dcf7d04ae1d950d86039a159a9
David <denton.david@gmail.com>
2023-03-13 21:05:54 +0000

    adding code for various slides

--- .presentation/src/testFixtures/kotlin/actor.kt
+++ .presentation/src/testFixtures/kotlin/actor.kt
@@ -1,23 +1,26 @@
 import hyperpyramid.actor.Actor
 import hyperpyramid.dto.ItemId
+import hyperpyramid.dto.OrderId
 import org.http4k.core.HttpHandler
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
+import org.http4k.core.Request
 import org.http4k.core.Uri
+import org.http4k.core.then
 import org.http4k.events.Events
-import org.http4k.webdriver.Http4kWebDriver
-import org.openqa.selenium.By
+import org.http4k.filter.ClientFilters.SetBaseUriFrom
 
 class Customer(http: HttpHandler, private val baseUri: Uri, events: Events = {}) :
     Actor("Website User", http, events) {
 
-    private val browser = Http4kWebDriver(this.http)
+    fun listItems() = SetBaseUriFrom(baseUri).then(http)(Request(GET, "/list"))
 
-    fun listItems() = with(browser) {
-        navigate().to(baseUri)
-        (findElements(By.tagName("form")) ?: emptyList())
-            .map { ItemId.of(it.getAttribute("action").substringAfterLast('/')) }
-    }
+    fun order(id: ItemId) =
+        SetBaseUriFrom(baseUri).then(http)(Request(POST, "/order/$id"))
+            .let { OrderId.parse(it.bodyString()) }
 }
 
 fun main() {
     Customer(TODO(), TODO(), TODO()).listItems()
+    Customer(TODO(), TODO(), TODO()).order(ItemId.of("!23"))
 }
