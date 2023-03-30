3ece6ab848a7709ebe1a62d2900699c1b63abe3e
David <denton.david@gmail.com>
2023-03-30 14:56:22 +0100

    internet is an HttpHandler

--- shop/src/testFixtures/kotlin/hyperpyramid/FakeWarehouse.kt
+++ shop/src/testFixtures/kotlin/hyperpyramid/FakeWarehouse.kt
@@ -4,17 +4,17 @@ import hyperpyramid.dto.InventoryItem
 import hyperpyramid.dto.ItemId
 import hyperpyramid.util.Json.auto
 import org.http4k.core.Body
+import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.ACCEPTED
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
-import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 
-fun FakeWarehouse(): RoutingHttpHandler {
+fun FakeWarehouse(): HttpHandler {
     val stockLevels = mutableMapOf(ItemId.of("1") to 5)
 
     return routes(
